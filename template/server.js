7ce430c1e72dd8a4c92081df8431f1f30219f166
Linqing Lu <lulinqing@gmail.com>
2013-09-03 00:08:39 +0800

    1

--- template/server.js
+++ template/server.js
@@ -53,9 +53,9 @@ var StrongLoopApp = function() {
     } catch(err) {
     }
 
-    self.ipaddress = process.env.STRONGLOOP_HOST ||
+    self.ipaddress = process.env.OPENSHIFT_NODEJS_IP ||
                      process.env.VCAP_HOST || '0.0.0.0';
-    self.port      = process.env.STRONGLOOP_PORT ||
+    self.port      = process.env.OPENSHIFT_NODEJS_PORT ||
                      process.env.VCAP_PORT  || process.env.PORT || 3000;
     self.app_name  = cfg.name || 'StrongLoop-Sample-App';
 
