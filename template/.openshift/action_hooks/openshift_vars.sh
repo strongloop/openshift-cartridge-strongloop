3e077482e1c66009e5d74468ba0d6c32520d8d23
Linqing Lu <lulinqing@gmail.com>
2013-09-02 21:27:36 +0800

    add ignored libs

---
+++ template/.openshift/action_hooks/openshift_vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#  TODO: Change this on release - needs a publicly accessible github
+#        repo for testing.
+github_repo="git://github.com/strongloop/dist-paas-buildpack.git"
+
+install_dir="$OPENSHIFT_DATA_DIR"
+cache_dir="$OPENSHIFT_DATA_DIR/strongloop/.cache"
+
