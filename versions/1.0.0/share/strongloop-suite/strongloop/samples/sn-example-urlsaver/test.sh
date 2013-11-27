538f4985f73e2779f50999f5adb003912591efb9
cgole <chandrika@strongloop.com>
2013-11-27 15:19:28 -0800

    Deleted everything under share/

--- versions/1.0.0/share/strongloop-suite/strongloop/samples/sn-example-urlsaver/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -X PUT http://localhost:1337/sfmeetup?url=http://blog.strongloop.com/improving-meetups-as-the-new-organizer-of-the-sf-node-js-group/
-echo
-curl http://localhost:1337/sfmeetup
